8f7f608f23f7ce565622e482d9853957d36db966
thallysonpontes <thallysonsalazar@hotmail.com>
2020-05-18 02:37:59 -0300

    Incluido atualizar cliente

--- curso-spring-boot-especialista-03_01_criando_controller_de_clientes/src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
+++ curso-spring-boot-especialista-03_01_criando_controller_de_clientes/src/main/java/io/github/dougllasfps/rest/controller/ClienteController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,4 +44,28 @@ public class ClienteController {
 		Cliente clienteSalvo = clientes.save(cliente);
 		return ResponseEntity.ok(clienteSalvo);
 	}
+	
+	@DeleteMapping("/api/clientes/{id}")
+	@ResponseBody
+	public ResponseEntity delete(@PathVariable Integer id) {
+		Optional<Cliente> cliente = clientes.findById(id);
+		if (cliente.isPresent()) {
+			clientes.delete(cliente.get());
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PutMapping("/api/clientes/{id}")
+	@ResponseBody
+	public ResponseEntity update(@PathVariable Integer id,
+								 @RequestBody Cliente cliente) {
+		return clientes.findById(id).map(
+				clienteExistente -> {
+				cliente.setId(clienteExistente.getId());
+				clientes.save(cliente);
+				return ResponseEntity.noContent().build();
+	}).orElseGet(() -> ResponseEntity.notFound().build());
+	}
 }
+
